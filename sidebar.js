ccb0b7ce44fc13910a46aaa8b75329becfd36fd3
ryanb004 <112939771+ryanb004@users.noreply.github.com>
2023-09-22 19:11:39 +1000

    t

--- sidebar.js
+++
@@ -1,9 +0,0 @@
-sidebarButton = document.getElementById("sidebar")
-sidebar = document.querySelector(".sidebar-content")
-sidebarButton.addEventListener("click", () => {
-    sidebar.style = "width:250px"
-})
-content = document.querySelector(".main-content")
-content.addEventListener("click", () =>{
-    sidebar.style = "width:0px"
-})
